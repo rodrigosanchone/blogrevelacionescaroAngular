c0869bda0665cdbe49db8857481678d4feb659cc
unknown <rodrigosanchonet@outlook.es>
2024-05-17 11:37:28 -0600

    chaces

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -6,8 +6,37 @@ import { ThemeService } from '../../services/theme.services';
 import { Meta, Title } from '@angular/platform-browser';
 
 
+import { InlineShareButtonsConfig } from "sharethis-angular";
 
+const inlineShareButtonsConfig: InlineShareButtonsConfig = {
+  alignment: "center", // alignment of buttons (left, center, right)
+  color: "social", // set the color of buttons (social, white)
+  enabled: true, // show/hide buttons (true, false)
+  font_size: 16, // font size for the buttons
+  labels: "cta", // button labels (cta, counts, null)
+  language: "en", // which language to use (see LANGUAGES)
+  networks: [
+    // which networks to include (see SHARING NETWORKS)
+    "whatsapp",
+    "linkedin",
+    "messenger",
+    "facebook",
+    "twitter",
+  ],
+  padding: 12, // padding within buttons (INTEGER)
+  radius: 4, // the corner radius on each button (INTEGER)
+  show_total: true,
+  size: 40, // the size of each button (INTEGER)
 
+  // OPTIONAL PARAMETERS
+  url: "https://www.sharethis.com", // (defaults to current url)
+  image: "https://bit.ly/2CMhCMC", // (defaults to og:image or twitter:image)
+  description: "custom text", // (defaults to og:description or twitter:description)
+  title: "custom title", // (defaults to og:title or twitter:title)
+  message: "custom email text", // (only for email sharing)
+  subject: "custom email subject", // (only for email sharing)
+  username: "custom twitter handle", // (only for twitter sharing)
+};
 
 
 @Component({
@@ -16,8 +45,7 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent  implements OnInit {
-
-
+  config: any;
 
   id: string = ""
   change: boolean = true;
@@ -59,11 +87,43 @@ export class ArticleComponent  implements OnInit {
           this.metaService.updateTag({ name: 'description', content: article.titulo });
           this.metaService.updateTag({ name: 'image', content: article.img });
           this.metaService.updateTag({ name: 'url', content: `https://revelacionesdecaro.com/articulo/${article.id}` });
+           
+          this.config={
+            alignment: "center", // alignment of buttons (left, center, right)
+            color: "social", // set the color of buttons (social, white)
+            enabled: true, // show/hide buttons (true, false)
+            font_size: 16, // font size for the buttons
+            labels: "cta", // button labels (cta, counts, null)
+            language: "en", // which language to use (see LANGUAGES)
+            networks: [
+              // which networks to include (see SHARING NETWORKS)
+              "whatsapp",
+              "linkedin",
+              "messenger",
+              "facebook",
+              "twitter",
+            ],
+            padding: 12, // padding within buttons (INTEGER)
+            radius: 4, // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 40, // the size of each button (INTEGER)
+          
+            // OPTIONAL PARAMETERS
+            url: `https://revelacionesdecaro.com/articulo/${article.id}`, // (defaults to current url)
+            image: this.article.img, // (defaults to og:image or twitter:image)
+            description: this.article.titulo, // (defaults to og:description or twitter:description)
+            title: this.article.titulo, // (defaults to og:title or twitter:title)
+            message: "custom email text", // (only for email sharing)
+            subject: "custom email subject", // (only for email sharing)
+            username: "custom twitter handle", // (only for twitter sharing)
+          };
           
         } else {
           // Redirige a la página 404 si el artículo no existe
           this.router.navigate(['/404']);
         }
+
+        
 
       });
     });
